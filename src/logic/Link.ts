9b351043ac27e8e3f3095c838cf23fa188c45282
rkoniushenko <romaka97@gmail.com>
2024-03-22 14:52:45 +0200

    Added jest config; Added test as an example

--- src/logic/Link.ts
+++ src/logic/Link.ts
@@ -1,25 +1,26 @@
 import {TDomain, THash, TOriginalUrl, TPath, TProtocol, TQuery} from '../types/link';
 
-class Link {
+export default class Link {
+    private _protocol: TProtocol | undefined;
+    private _domain: TDomain | undefined;
+    private _path: TPath | undefined | null;
+    private _query: TQuery | undefined | null;
+    private _hash: THash | undefined | null;
     constructor(
         private _originalUrl: TOriginalUrl,
-        private _protocol: TProtocol,
-        private _domain: TDomain,
-        private _path?: TPath,
-        private _query?: TQuery,
-        private _hash?: THash
     ) {
+        this.parseUrl()
     }
 
     get original(): TOriginalUrl {
         return this._originalUrl;
     }
 
-    get protocol(): TProtocol {
+    get protocol(): TProtocol | undefined {
         return this._protocol;
     }
 
-    get domain(): TDomain {
+    get domain(): TDomain | undefined {
         return this._domain;
     }
 
@@ -33,6 +34,18 @@ class Link {
 
     get hash(): THash | undefined | null{
         return this._hash;
+    }
+
+    private parseUrl(): void {
+        if (!this._originalUrl) {
+            throw new Error('Original URL is not set');
+        }
+        const url = new URL(this._originalUrl);
+        this._protocol = url.protocol;
+        this._domain = url.hostname;
+        this._path = url.pathname;
+        this._query = url.search;
+        this._hash = url.hash;
     }
 
     //TODO implement method to generate a short link Id based on base62 @kcfz
